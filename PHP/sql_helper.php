0eb13efbcc61e7f517f31582b66b9ddc218abc1b
siaris <wirawan.is@gmail.com>
2022-03-02 11:24:21 +0700

    betulin nama fungsi

--- PHP/sql_helper.php
+++ PHP/sql_helper.php
@@ -18,7 +18,7 @@ function set_order_exactmatch($field='name',$str_exact='',$order_type_after_rlik
     return 'ORDER BY '.$field.' RLIKE "'.$str_exact.'" DESC, '.$field.' '.$order_type_after_rlike;
 }
 
-function neste_replace_mysql($field,$arr_char_to_replace=array(''),$arr_replace_by=array('')){
+function nested_replace_mysql($field,$arr_char_to_replace=array(''),$arr_replace_by=array('')){
     $return = $field;
     foreach($arr_char_to_replace as $key=>$to_replace){
         $return = 'REPLACE('.$return.',"'.$arr_char_to_replace[$key].'","'.$arr_replace_by[$key].'")';
